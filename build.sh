2771e3532a0d11be245b51c7a5fa1012f90c6974
poti <poti.dog@gmail.com>
2021-04-10 19:46:25 +0900

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #/bin/bash
 
+cd protocol && protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative protocol.proto && cd ..
 CGO_LDFLAGS="`mecab-config --libs`" go build
-cd client && go build
+cd client && go build && cd ..
